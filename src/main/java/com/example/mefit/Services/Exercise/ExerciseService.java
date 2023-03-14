b3a5028e3c9cb27421a2c44fd27c8f2d387edec3
konkokonos <73117962+konkokonos@users.noreply.github.com>
2023-03-14 16:40:53 +0200

    Delete src/main/java/com/example/mefit/Services directory

--- src/main/java/com/example/mefit/Services/Exercise/ExerciseService.java
+++
@@ -1,7 +0,0 @@
-package com.example.mefit.Services.Exercise;
-
-import com.example.mefit.Models.Exercise;
-import com.example.mefit.Services.CrudService;
-
-public interface ExerciseService extends CrudService<Exercise,Integer> {
-}
--- src/main/java/com/example/mefit/Services/User/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.mefit.Services.User;
-
-import com.example.mefit.Models.User;
-import com.example.mefit.Services.CrudService;
-
-public interface UserService  extends CrudService<User,Integer> {
-
-}
