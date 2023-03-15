8ccb22ea8981dac7aac32f0f322d7bb6d45d2c5b
f.staikos7 <f.staikos7@gmail.com>
2023-03-15 18:10:23 +0200

    feat: Added data.sql entries

--- src/main/java/com/example/mefit/Models/Workout.java
+++ src/main/java/com/example/mefit/Models/Workout.java
@@ -19,7 +19,7 @@ public class Workout {
     @Column(name = "workout_type",length = 50,nullable = false)
     private String type;
     @Column(name = "workout_complete")
-    private Boolean complete_workout;
+    private Boolean complete;
 
     @JsonIgnore
     @ManyToMany(mappedBy = "workout")
