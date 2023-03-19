85522777ab0a5d5ed1e8d5d9907caef6ae227b52
f.staikos7 <f.staikos7@gmail.com>
2023-03-19 16:01:15 +0200

    fix: fixed services

--- src/main/java/com/example/mefit/Controllers/ExerciseController.java
+++ src/main/java/com/example/mefit/Controllers/ExerciseController.java
@@ -18,10 +18,9 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collection;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(path = "api/v1/exercise")
-
 public class ExerciseController {
     private final ExerciseService exerciseService;
     private final ExerciseMapper exerciseMapper;
