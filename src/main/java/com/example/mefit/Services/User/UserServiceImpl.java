f2b4a6b28c633b161909bccd9ea2beac8b2b240e
konkokonos <73117962+konkokonos@users.noreply.github.com>
2023-03-27 17:17:34 +0300

    Create Profile when creating a new User
    Create Profile when creating a new User

--- src/main/java/com/example/mefit/Services/User/UserServiceImpl.java
+++ src/main/java/com/example/mefit/Services/User/UserServiceImpl.java
@@ -2,19 +2,29 @@ package com.example.mefit.Services.User;
 
 import com.example.mefit.Exceptions.UserAlreadyExistsException;
 import com.example.mefit.Exceptions.UserNotFoundException;
+import com.example.mefit.Mappers.ProfileMapper;
+import com.example.mefit.Models.AppUser;
+import com.example.mefit.Models.DTO.ProfileDTO;
 import com.example.mefit.Models.Profile;
-import com.example.mefit.Models.User;
 import com.example.mefit.Repositories.UserRepository;
+import com.example.mefit.Services.Profile.ProfileService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final ProfileService profileService;
+    private final ProfileMapper profileMapper;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, ProfileService profileService, ProfileMapper profileMapper) {
         this.userRepository = userRepository;
+        this.profileService = profileService;
+        this.profileMapper = profileMapper;
     }
 
     @Override
@@ -43,6 +53,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User add(User entity) {
+        ProfileDTO newProfile = new ProfileDTO();
+        newProfile.setUser(uid);
+        Profile addedProfile = profileService.add(profileMapper.profileDtoToProfile(newProfile));
+        ProfileDTO addedProfileDTO = profileMapper.profileToProfileDTO(addedProfile);
+        URI uri = new URI("api/v1/profile/" + addedProfileDTO.getId());
+        //return ResponseEntity.created(uri).body(addedProfileDTO);
         return userRepository.save(entity);
     }
 
