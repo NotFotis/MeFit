900dd0cb9ec75b63b43daa461fe6eb5aae30ffd2
f.staikos7 <f.staikos7@gmail.com>
2023-03-28 14:07:21 +0300

    added more cross origins

--- src/main/java/com/example/mefit/Controllers/ProfileController.java
+++ src/main/java/com/example/mefit/Controllers/ProfileController.java
@@ -23,7 +23,7 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping(path = "api/v1/profile")
-@CrossOrigin(origins = {"http://localhost:3000/", "https://my-fit-one.vercel.app/*", "https://my-fit-one.vercel.app/**","https://*.vercel.app/*"},
+@CrossOrigin(origins = {"http://localhost:3000/", "https://my-fit-one.vercel.app/*", "https://my-fit-one.vercel.app","https://*.vercel.app/*","*"},
         allowedHeaders = { "Origin", "Accept", "X-Requested-With", "Content-Type",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization" },
         exposedHeaders = { "Access-Control-Allow-Origin",
--- src/main/java/com/example/mefit/Controllers/UserController.java
+++ src/main/java/com/example/mefit/Controllers/UserController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/users")
-@CrossOrigin(origins = {"http://localhost:3000/", "https://my-fit-one.vercel.app/*", "https://my-fit-one.vercel.app/**","https://*.vercel.app/*"},
+@CrossOrigin(origins = {"http://localhost:3000/", "https://my-fit-one.vercel.app/*", "https://my-fit-one.vercel.app","https://*.vercel.app/*","*"},
         allowedHeaders = { "Origin", "Accept", "X-Requested-With", "Content-Type",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization" },
         exposedHeaders = { "Access-Control-Allow-Origin",
